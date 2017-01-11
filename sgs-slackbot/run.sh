491e2c2cfb4665f5e990a858376f008248942c4a
Simon Mulser <simonmulser@gmail.com>
2017-01-11 20:13:58 +0100

    remove unneeded sgs directory

--- sgs-slackbot/run.sh
+++ sgs-slackbot/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "start sgs-slackbot"
 export PATH=$PATH:/go/bin:/usr/local/go/bin
-cd /go/src/github.com/simonmulser/sgs/sgs-slackbot/
+cd /go/src/github.com/simonmulser/sgs-slackbot/
 sgs-slackbot production
 echo "sgs-slackbot terminated"
